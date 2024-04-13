bc16e37949cc794e3c9bd8b751f80f4ea7759987
Nikola Dojcinovic <goonergalactico@gmail.com>
2024-04-13 18:55:37 +0200

    first commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,10 @@
+import { HeroHighlight } from "@/components/ui/hero-highlight";
+import { Card } from "./Card";
+
+export function Hero() {
+  return (
+    <HeroHighlight>
+      <Card />
+    </HeroHighlight>
+  );
+}
